0e89423222ca9f0604dc4a2ef87d7aab043b92da
Valerii Zaichenko <wellestonia@gmail.com>
2024-05-08 15:59:23 +0300

    Added HW classes

---
+++ Homework_13. Classes/script_classes_task_1.py
@@ -0,0 +1,23 @@
+class Country:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+        self.new_country = [name, population]
+
+
+    def add(self, other_country):
+        new_name = self.name + ' - ' + other_country.name
+        new_population = self.population + other_country.population
+        new_country = Country(new_name, new_population) 
+        return new_country
+    
+
+    
+bosnia = Country('Bosnia', 10_000_000)
+herzegovina = Country('Herzegovina', 5_000_000)
+bosnia_herzegovina = bosnia.add(herzegovina)
+
+print(bosnia_herzegovina.name)
+print(bosnia_herzegovina.population)
+    
+
