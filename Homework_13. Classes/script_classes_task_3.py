ea1b5f811b4b4e5034ac54fe9679cc57891547e5
Valerii Zaichenko <wellestonia@gmail.com>
2024-05-08 16:05:08 +0300

    Added optional task

--- Homework_13. Classes/script_classes_task_3.py
+++ Homework_13. Classes/script_classes_task_3.py
@@ -1,44 +1,22 @@
-# The move method should take a number of steps and move the robot in the direction it is currently facing. 
-# The turn method should take a direction (left or right) and turn the robot in that direction.
-# The display_position method should print the current position of the robot.
+class Car:
+    def __init__(self, brand, model, year, speed):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.speed = speed
 
+    def accelerate(self):
+        self.speed += 5
+        return self.speed
 
-class Robot:
-    def __init__(self, position_x, position_y):
-        self.position_x = position_x
-        self.position_y = position_y
-        self.orientation = 'up'
+    def brake(self):
+        self.speed -= 5
 
-    def turn(self, direction):
-        if direction == 'right':
-            if self.orientation == 'up':
-                self.orientation = 'right'
-            elif self.orientation == 'right':
-                self.orientation = 'down'
-            elif self.orientation == 'down':
-                self.orientation = 'left'
-            else:
-                self.orientation = 'up'
-        elif direction == 'left':
-            if self.orientation == 'up':
-                self.orientation = 'left'
-            elif self.orientation == 'left':
-                self.orientation = 'down'
-            elif self.orientation == 'down':
-                self.orientation = 'right'
-            else:
-                self.orientation = 'up'        
+        if self.speed < 0:
+            self.speed = 0
 
-    def move(self, steps):
-        if self.orientation == 'up':
-            self.position_y += steps
-        elif self.orientation == 'down':
-            self.position_y -= steps
-        elif self.orientation == 'left':
-            self.position_x -= steps
         else:
-            self.position_x += steps
-
-    def display_position(self):
-        print(f'Current position is :{self.position_x},{self.position_y}, direction is {self.orientation}') 
-
+            return self.speed
+        
+    def display_speed(self):
+        return self.speed
